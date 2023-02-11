b88fa94c426233559d64de1a124b6e4d5d82f5d3
OleksiyMunenko <oleksiymun@gmail.com>
2023-02-12 01:44:14 +0200

    task 03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+const submitBtn = document.querySelector('button')
+const KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(savingData, 500));
+submitBtn.addEventListener('click', onFormSubmit);
+
+onSiteReload();
+
+
+function savingData(form){
+	const inputValue = {
+	email: input.value,
+	message: textarea.value,
+	};	
+ localStorage.setItem(KEY, JSON.stringify(inputValue));
+
+};
+
+function onSiteReload() {
+	let savedData = localStorage.getItem(KEY);
+	let parsedData = JSON.parse(savedData);
+	if(parsedData !== null){
+		input.value = parsedData.email;
+		textarea.value = parsedData.message;
+	}
+
+}
+
+function onFormSubmit(e) {
+	e.preventDefault();
+	localStorage.removeItem(KEY);
+	form.reset(); 	
+}
 
