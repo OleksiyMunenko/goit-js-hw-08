6be3bc320f3c6dd3dcd018cb7847c42d4ca4781e
OleksiyMunenko <oleksiymun@gmail.com>
2023-02-12 00:19:57 +0200

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(galleryItems) {
+	return galleryItems.map(({preview, original, description}) => {
+		return `
+		<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	 </a>`
+	}).join('');
+
+};
+
+new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
+
