7dbf5ce210bffa47bf767b503066ec3f33eda325
OleksiyMunenko <oleksiymun@gmail.com>
2023-02-12 00:27:02 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import playerVimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new playerVimeo(iframe);
+
+const KEY = 'videoplayer-current-time';
+
+videoTimer();
+
+const setPlayer = throttle(function (iframe) {
+  localStorage.setItem(KEY, `${iframe.seconds}`);
+}, 1000);
+
+player.on('timeupdate', setPlayer);
+
+function videoTimer() {
+  const timerStorage = localStorage.getItem(KEY);
+  if (timerStorage) {
+    player.setCurrentTime(localStorage.getItem(KEY));
+  }
+}
 
